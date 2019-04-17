1aaaf96829fa0921b94dca4e37da22a3c793608e
gupps17 <41826312+gupps17@users.noreply.github.com>
2019-04-17 15:52:08 +0530

    Add files via upload

---
+++ Fullfiller.java
@@ -0,0 +1,55 @@
+package com.amazonaws.aws_java_sdk;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTypeConverted;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTypeConverter;
+
+class Fullfiller 
+
+    {   public ArrayList<Integer> m=new ArrayList<Integer>();
+    	
+    public void setQty(Integer Qty)
+    	{
+    		m.add(Qty);
+            
+    	}
+    public int getQty()
+	{
+		return m.get(0);
+        
+	}
+    
+    public void updateQty(int i,int q)
+    {
+    	m.set(i,q);
+    }
+    public void updateid(int i,int id)
+    {
+    	m.set(i,id);
+    }
+    	
+    
+    	
+    	public void setid(Integer id)
+    	{
+    		m.add(id);
+            
+    	}
+    	 public int getid()
+    	{
+    			return m.get(1);
+    	        
+    	}
+    	    	
+    	
+    	
+    	public String toString()
+    	{
+    	    return m+"";
+    	}
+    
+    }
